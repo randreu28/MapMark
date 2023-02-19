835f68e5d1453f49fcb95f298334a2e8da381390
Rubén Chiquin <randreu28@gmail.com>
2023-02-19 20:56:58 +0100

    Creates a simple map using mapbox

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:latlong2/latlong.dart';
 
 import 'secrets.dart';
 
@@ -17,7 +19,33 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.dark(),
-      home: const Scaffold(),
+      home: Scaffold(
+          floatingActionButton: const FloatingActionButton(
+              onPressed: null, child: Icon(Icons.camera_alt)),
+          body: Stack(
+            children: [
+              FlutterMap(
+                options: MapOptions(
+                  minZoom: 5,
+                  maxZoom: 16,
+                  zoom: 13,
+                  center: LatLng(41.429795, 2.194170),
+                ),
+                nonRotatedChildren: const [
+                  //TODO
+                ],
+                children: [
+                  TileLayer(
+                    urlTemplate:
+                        "https://api.mapbox.com/styles/v1/randreu28/clebruq3o001201r15xtwdylg/tiles/256/{z}/{x}/{y}@2x?access_token={access_token}",
+                    additionalOptions: const {
+                      "access_token": mapboxKey,
+                    },
+                  ),
+                ],
+              )
+            ],
+          )),
     );
   }
 }
