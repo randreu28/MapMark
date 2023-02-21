633180c8d90da13b704981a42c2fef5cb32d7faa
Rubén Chiquin <randreu28@gmail.com>
2023-02-21 17:09:50 +0100

    Creates a list of mapathons to select from

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:mapmark/models.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:intl/intl.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final db = Supabase.instance.client;
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +19,43 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: const Text("MapMark"),
       ),
-      body: const Center(child: Text("Home")),
+      body: StreamBuilder(
+        stream: db.from('mapathons').select().asStream(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text("Error: ${snapshot.error}"));
+          }
+          if (snapshot.hasData == false) {
+            return const Center(child: Text("Loading..."));
+          } else {
+            List<Mapathon> mapathons = [];
+
+            for (dynamic rawMapathon in snapshot.data) {
+              mapathons.add(Mapathon.fromJson(rawMapathon));
+            }
+
+            mapathons = List.from(mapathons.where(
+              (mapathon) => mapathon.hasEnded,
+            ));
+
+            return ListView.builder(
+              itemCount: mapathons.length,
+              itemBuilder: (context, index) {
+                final mapathon = mapathons[index];
+                final dateFormat = DateFormat('dd/MM/yyyy');
+
+                return ListTile(
+                  title: Text(mapathon.name),
+                  subtitle: Text(
+                    "From ${dateFormat.format(mapathon.startDate)} to ${dateFormat.format(mapathon.endDate)}",
+                  ),
+                  textColor: mapathon.hasStarted ? Colors.grey : null,
+                );
+              },
+            );
+          }
+        },
+      ),
     );
   }
 }
