9935025323724bcc905b4eac091c79f0fd533adf
Rubén Chiquin <randreu28@gmail.com>
2023-03-05 13:43:27 +0100

    Adds a title and description to the point model
    Perhaps in the future there could be a category set by the mapathon director

--- lib/models.dart
+++ lib/models.dart
@@ -37,18 +37,25 @@ class Point {
   DateTime createdAt;
   LatLng coordinates;
   String picture;
+  String title;
+  String description;
 
-  Point(
-      {required this.id,
-      required this.mapathonID,
-      required this.createdAt,
-      required this.coordinates,
-      required this.picture});
+  Point({
+    required this.id,
+    required this.mapathonID,
+    required this.createdAt,
+    required this.coordinates,
+    required this.picture,
+    required this.title,
+    required this.description,
+  });
 
   Point.fromJson(dynamic data)
       : id = data["id"],
         mapathonID = data["mapathon"],
         createdAt = DateTime.parse(data["created_at"]),
         coordinates = LatLng(data["latitude"], data["longitude"]),
-        picture = data["picture"];
+        picture = data["picture"],
+        title = data["title"],
+        description = data["description"];
 }
